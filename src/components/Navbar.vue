b068ccc36f4505805091247f3e62013d4aaa17f6
Mehmet Can Girgin <mcangirgin34@gmail.com>
2024-06-02 14:41:46 +0300

    added form

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,8 @@
             <a class="navbar-brand" href="#">My Vue</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li v-for="(page, index) in pages" class="nav-item" :key="index">
-                    <a class="nav-link" :class="{ active: activePage == index }" aria-current="page"
-                        v-bind:href="page.link.url" :title="`This link goes to the ${page.link.text} page`"
-                        v-on:click.prevent="navLinkClick(index)">{{ page.link.text }}</a>
+                    <navbar-link :page="page" :isActive="activePage === index"
+                        v-on:click.prevent="navLinkClick(index)"></navbar-link>
                 </li>
             </ul>
             <form class="d-flex">
@@ -19,7 +18,13 @@
 </template>
 
 <script>
+import NavbarLink from './NavbarLink.vue';
+
 export default {
+    components: { NavbarLink },
+    created() {
+        this.getThemeSetting();
+    },
     props: ["pages", "activePage", "navLinkClick"],
     data() {
         return {
@@ -35,6 +40,17 @@ export default {
             }
 
             this.theme = theme;
+            this.storeThemeSetting();
+        },
+        storeThemeSetting() {
+            localStorage.setItem('theme', this.theme);
+        },
+        getThemeSetting() {
+            let theme = localStorage.getItem('theme');
+
+            if (theme) {
+                this.theme = theme;
+            }
         },
     },
 }
